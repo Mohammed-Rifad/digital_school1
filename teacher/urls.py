45100c541a452f3cfa9d118ccf449ae1bbeb999e
Mohammed-Rifad <mohammedrifad17@gmail.com>
2022-09-01 11:02:44 +0530

    first

--- teacher/urls.py
+++ teacher/urls.py
@@ -3,5 +3,10 @@ from . import views
 urlpatterns=[
 
     path('auth-check',views.teacher_auth),
-    path('view-teachers',views.view_teachers)
+    path('view-teachers',views.view_teachers),
+    path('add-student',views.add_student),
+    path('load-students',views.load_students),
+    path('delete-student/<int:id>',views.delete_student),
+    path('update-student/<int:id>',views.update_student)
+
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -1,10 +1,11 @@
+from logging import exception
 from multiprocessing import managers
 from xmlrpc.client import ResponseError
 from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import TeacherSerializer
+from .serializers import StudentSerializer, TeacherSerializer
 from .models import *
 
 from django.views.decorators.csrf import csrf_exempt
@@ -15,17 +16,15 @@ from django.views.decorators.csrf import csrf_exempt
  
 def teacher_auth(request):
     
-    print('llllllllllllllllllll')
+     
     params=request.data
     statusCode=0
     email=params['email']
     passwd=params['passwd']
-    print(email,passwd)
+    
     try:
         login_check=Teacher.objects.get(email=email,password=passwd)
-        print('00000000000000000000000',login_check)
         statusCode=200
-        print('heeererrer')
     except:
         statusCode=401
         return Response({'statusCode':statusCode})
@@ -37,3 +36,85 @@ def view_teachers(request):
     teachers=Teacher.objects.all()
     ser=TeacherSerializer(teachers,many=True)
     return Response(ser.data)
+
+@api_view(['POST'])
+def add_student(request):
+    params=request.data
+    statusCode=0
+
+    s_name = params['s_name'] 
+    s_email = params['s_email'] 
+    s_gender = params['s_gender'] 
+    s_phone = params['s_phone'] 
+    s_address = params['s_address']
+    s_username = params['s_username']  
+    s_passwd = params['s_passwd'] 
+
+    try:
+        exist =Student.objects.filter(s_email = s_email).exists()
+
+        if not exist :
+
+
+            student = StudentSerializer(data = params)
+
+            if student.is_valid():
+
+                student.save()
+
+                statusCode= 200
+        else : 
+            statusCode = 409
+
+    except Exception as e:
+        print(e)
+
+        statusCode=401  
+    
+    return Response({'statusCode':statusCode})
+
+@api_view(['GET'])
+def load_students(request):
+    
+    students=Student.objects.all()
+    ser=StudentSerializer(students,many=True)
+    return Response(ser.data)
+
+@api_view(['GET'])
+def delete_student(request,id):
+     
+    statusCode= 0 
+     
+
+    try:
+        student = Student.objects.get(id = id)
+        student.delete()
+        statusCode = 200
+    
+    except:
+        statusCode = 409
+        pass
+
+    return Response({'statusCode':statusCode})
+
+
+@api_view(['PUT'])
+def update_student(request,id):
+    # params=request.data
+    statusCode=0
+
+    try:
+        student = Student.objects.get(id = id)
+        serializer = StudentSerializer(student, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            statusCode = 200
+        else:
+            pass
+        
+    except Exception as e:
+        print(e)
+        statusCode = 409
+        pass
+
+    return Response({'statusCode':statusCode})
